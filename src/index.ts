4db15fb48e94ff5fa098b4c7b9c87ab16838d804
Camillemtd <camille_mtd@hotmail.com>
2023-12-12 14:28:26 -0500

    prod

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,11 @@ const rateLimit = require('express-rate-limit');
 const app = express();
 
 
-app.use(cors());
 
+const corsOptions = {
+  origin: 'http://localhost:5173', // Remplacez par l'URL de votre frontend en production si nécessaire
+};
+app.use(cors(corsOptions));
 app.use(express.json()); 
 
 app.use('/api/auth', authRoutes);
