6c0e0f26a48b40dbaaed8e592736108c67064f8f
Camillemtd <camille_mtd@hotmail.com>
2023-12-12 18:26:24 -0500

    prod

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 
 
 const corsOptions = {
-  origin: 'http://localhost:5173', // Remplacez par l'URL de votre frontend en production si nécessaire
+  origin: 'http://localhost:5173', 
 };
 
 app.use(cors(corsOptions));
