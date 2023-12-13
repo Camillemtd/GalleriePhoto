cb9eb260c851f34bc83125659fa4622e4401e14f
Camillemtd <camille_mtd@hotmail.com>
2023-12-12 21:12:48 -0500

    prod

--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,18 @@ const rateLimit = require('express-rate-limit');
 
 const app = express();
 
-
-
 const corsOptions = {
-  origin: 'http://localhost:5173', 
+  origin: 'http://localhost:5173', // Et ajoutez ici l'URL de votre front-end en production
 };
 
-
-app.use(express.json()); 
+app.use(cors(corsOptions));
+app.use(express.json());
 
 app.use('/api/auth', authRoutes);
-
 app.use('/api/pictures', pictureRoutes);
-app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Serveur en écoute sur le port ${PORT}`);
 });
+
